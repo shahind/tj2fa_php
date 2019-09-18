9058b7fb8c8b59683f72c2eccba7aa46bb854491
shahind <shahin.shdm@gmail.com>
2019-09-18 23:55:26 +0430

    Beta 1.0.1

--- tj2fa.php
+++ tj2fa.php
@@ -3,19 +3,25 @@
  function tj2fa($tj_text){
      $fa_word = '';
      $fa_text = '';
+     $tj_text = correct_especial_words($tj_text);
      $words = splite_words($tj_text);
      foreach($words as $word){
          for($i = 0; $i<mb_strlen($word); $i++){
              
-             if($i==0 || $i == mb_strlen($word)-1){
+             if($i==0){
                  $first = true;
              }else{
                  $first = false;
              }
-             if(is_samet(mb_substr($word,$i,1))){
-                 $char = change_samet(mb_substr($word,$i,1),$first);
+             if($i == mb_strlen($word)-1){
+                 $last = true;
              }else{
-                 $char = change_mosavvet(mb_substr($word,$i,1),$first);
+                 $last = false;
+             }
+             if(is_samet(mb_substr($word,$i,1))){
+                 $char = change_samet(mb_substr($word,$i,1),$first,$last);
+             }else{
+                 $char = change_mosavvet(mb_substr($word,$i,1),$first,$last);
              }
              $fa_word = $fa_word.''.$char;
          }
@@ -31,7 +37,7 @@
  }
  
  function is_samet($character){
-     $array = ['Б','б','В','в','Г','г','Ғ','ғ','Д','д','Ж','ж','З','з','К','к','Қ','қ','Л','л','М','м','Н','н','П','п','Р','р','С','с','Т','т','Ф','ф','Х','х','Ҳ','ҳ','Ч','ч','Ҷ','ҷ','Ш','ш','Ъ','ъ','Ё','ё','О','о','Ӯ','ӯ','И','и'];
+     $array = ['Б','б','В','в','Г','г','Ғ','ғ','Д','д','Ж','ж','З','з','К','к','Қ','қ','Л','л','М','м','Н','н','П','п','Р','р','С','с','Т','т','Ф','ф','Х','х','Ҳ','ҳ','Ч','ч','Ҷ','ҷ','Ш','ш','Ъ','ъ','Ё','ё','О','о'];
      if(in_array($character,$array)){
          return true;
      }else{
@@ -39,8 +45,8 @@
      }
  }
  
- function change_samet($char,$first){
-     $array = ['Б','б','В','в','Г','г','Ғ','ғ','Д','д','Ж','ж','З','з','К','к','Қ','қ','Л','л','М','м','Н','н','П','п','Р','р','С','с','Т','т','Ф','ф','Х','х','Ҳ','ҳ','Ч','ч','Ҷ','ҷ','Ш','ш','Ъ','ъ','Ё','ё','О','о','Ӯ','ӯ','И','и'];
+ function change_samet($char,$first,$last){
+     $array = ['Б','б','В','в','Г','г','Ғ','ғ','Д','д','Ж','ж','З','з','К','к','Қ','қ','Л','л','М','м','Н','н','П','п','Р','р','С','с','Т','т','Ф','ф','Х','х','Ҳ','ҳ','Ч','ч','Ҷ','ҷ','Ш','ш','Ъ','ъ','Ё','ё','О','о','И','и'];
      if($char==$array[0] || $char==$array[1]){
          return 'ب';
      }
@@ -130,10 +136,13 @@
      }
 }
  
- function change_mosavvet($char,$first){
+ function change_mosavvet($char,$first,$last){
      $array = ['А','а','Е','е','И','и','Ӣ','ӣ','Й','й','У','у','Ӯ','ӯ','Э','э','Ю','ю','Я','я'];
      if($first && ($char==$array[0] || $char==$array[1])){
          return 'ا';
+     }
+     if($last && ($char==$array[0] || $char==$array[1])){
+         return 'ه';
      }
      if($char==$array[0] || $char==$array[1]){
          return  '';
@@ -153,17 +162,44 @@
      if($first && ($char==$array[6] || $char==$array[7])){
          return 'ی';
      }
+     if($last && ($char==$array[6] || $char==$array[7])){
+         return 'ی';
+     }
+     if($char==$array[6] || $char==$array[7]){
+         return 'ی';
+     }
      if($first && ($char==$array[8] || $char==$array[9])){
-         return 'ا';
+         return 'ی';
+     }
+     if($last && ($char==$array[8] || $char==$array[9])){
+         return 'ی';
+     }
+     if($char==$array[8] || $char==$array[9]){
+         return 'ی';
      }
      if($first && ($char==$array[10] || $char==$array[11])){
          return 'و';
      }
+     if($last && ($char==$array[10] || $char==$array[11])){
+         return 'و';
+     }
+     if($char==$array[10] || $char==$array[11]){
+         return 'ُ';
+     }
      if($first && ($char==$array[12] || $char==$array[13])){
          return 'او';
      }
+     if($last && ($char==$array[12] || $char==$array[13])){
+         return 'و';
+     }
+     if($char==$array[12] || $char==$array[13]){
+         return 'و';
+     }
      if($first && ($char==$array[14] || $char==$array[15])){
-         return 'ا';
+         return 'ای';
+     }
+      if($char==$array[14] || $char==$array[15]){
+         return 'ی';
      }
      if($first && ($char==$array[16] || $char==$array[17])){
          return 'ا';
@@ -174,13 +210,24 @@
      if($first && ($char==$array[18] || $char==$array[19])){
          return 'ی';
      }
+     if($lsat && ($char==$array[18] || $char==$array[19])){
+         return 'یه';
+     }
      if($char==$array[18] || $char==$array[19]){
          return 'ی';
      }
-     if($char==$array[10] || $char==$array[11]){
-         return 'ُ';
-     }
+     
      return '';
  }
+ 
+ function correct_especial_words($text){
+     $text = str_replace(" ва "," в ",$text);
+     $text = str_replace(" вА "," в ",$text);
+     $text = str_replace(" Ба "," в ",$text);
+     $text = str_replace(" БА "," в ",$text);
+     return $text;
+ }
+ 
+
  
 ?>
