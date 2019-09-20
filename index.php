ccaeda754e2f2a65ab866af4ad34fe81c21eb21b
shahind <shahin.shdm@gmail.com>
2019-09-20 18:50:30 +0430

    version 1.2.0
    an spell corrector added

--- index.php
+++ index.php
@@ -5,8 +5,10 @@ $convertLatin2Cyr = false;
 if(isset($_POST['convertLatin2Cyr'])){
     $convertLatin2Cyr = true;
 }
-
-$fa_text = tj2fa($text,$convertLatin2Cyr);
+if(isset($_POST['spellCorrect'])){
+    $spellCorrect = true;
+}
+$fa_text = tj2fa($text,$convertLatin2Cyr,$spellCorrect);
 ?>
 
 <html>
@@ -19,12 +21,19 @@ $fa_text = tj2fa($text,$convertLatin2Cyr);
             <br>
             <input type="checkbox" name="convertLatin2Cyr" value="true"> تبدیل کاراکترهای مشابه لاتین به سیرلیک
             <br>
+            <input type="checkbox" name="spellCorrect" value="true"> اصلاح لهجه(آزمایشی)
+            <br>
             <input type="submit" value="تبدیل(табдил)"/>
             <br>
             <label>فارسی:</label>
             <br>
-            <label><?=$fa_text?></label>
+            <label style="background-color: Lavender;"><?=$fa_text?></label>
         </form>
+        <br>
+        <a href="https://github.com/shahind/tj2fa_php">PHP Source Code(Beta 1.0.3)
+        <br>
+        <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" width="75"/>
+        <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png" width="150"/></a>
         </center>
     </body>
 </html>
--- tj2fa.php
+++ tj2fa.php
@@ -1,6 +1,7 @@
 <?php
+include_once 'SpellCorrector.php';
 
- function tj2fa($tj_text,&$convertLatin2Cyr=false){
+ function tj2fa($tj_text,&$convertLatin2Cyr=false,&$spellCorrect=false){
      $fa_word = '';
      $fa_text = '';
      $tj_text = mb_strtolower($tj_text);
@@ -31,18 +32,24 @@
              }
              $fa_word = $fa_word.''.$char;
          }
+         $fa_word = correct_tashdid($fa_word);
+         $fa_word = filter_common_words($fa_word);
+         if($spellCorrect){
+                 $fa_word = remove_erab($fa_word);
+                 $fa_word = SpellCorrector::correct($fa_word);
+             }
          $fa_text = $fa_text.' '.$fa_word;
          $fa_word = '';
      }
      return $fa_text;
  }
  
- function splite_words($text){
+  function splite_words($text){
      $words = explode (" ", $text);  
      return $words; 
  }
  
- function is_samet($character){
+  function is_samet($character){
      $array = ['б','в','г','ғ','д','ж','з','к','қ','л','м','н','п','р','с','т','ф','х','ҳ','ч','ҷ','ш','ъ','ё','о'];
      if(in_array($character,$array)){
          return true;
@@ -60,7 +67,7 @@
      }
  }
  
- function change_samet($char,$first,$last){
+  function change_samet($char,$first,$last){
      $array = ['б','в','г','ғ','д','ж','з','к','қ','л','м','н','п','р','с','т','ф','х','ҳ','ч','ҷ','ш','ъ','ё','о'];
      if($char==$array[0]){
          return 'ب';
@@ -142,7 +149,7 @@
      }
 }
  
- function change_mosavvet($char,$first,$last){
+  function change_mosavvet($char,$first,$last){
      $array = ['а','е','и','ӣ','й','у','ӯ','э','ю','я'];
      if($first && $char==$array[0]){
          return 'ا';
@@ -226,13 +233,39 @@
      return '';
  }
  
- function correct_especial_words($text){
+  function correct_especial_words($text){
      $text = str_replace(" ва "," в ",$text);
+     
+     //ei to e
+     $text = str_replace("аи ","ҳ ӣ ",$text);
+     $text = str_replace("еи ","и ӣ ",$text);
+     $text = str_replace("би ","бе ",$text);
+     $text = str_replace("ги ","ге ",$text);
+     $text = str_replace("ғи ","ғе ",$text);
+     $text = str_replace("ди ","де ",$text);
+     $text = str_replace("жи ","же ",$text);
+     $text = str_replace("зи ","зе ",$text);
+     $text = str_replace("ки ","кҳ ",$text);
+     $text = str_replace("қи ","қе ",$text);
+     $text = str_replace("ли ","ле ",$text);
+     $text = str_replace("ми ","ме ",$text);
+     $text = str_replace("ни ","не ",$text);
+     $text = str_replace("пи ","пе ",$text);
+     $text = str_replace("ри ","ре ",$text);
+     $text = str_replace("си ","се ",$text);
+     $text = str_replace("ти ","те ",$text);
+     $text = str_replace("фи ","фе ",$text);
+     $text = str_replace("хи ","хе ",$text);
+     $text = str_replace("ҳи ","ҳе ",$text);
+     $text = str_replace("чи ","чҳ ",$text);
+     $text = str_replace("ҷи ","ҷҳ ",$text);
+     $text = str_replace("ши ","ше ",$text);
+     $text = str_replace("ъи ","ъҳ ",$text);
      
      return $text;
  }
  
- function correct_latin_characters($text){
+  function correct_latin_characters($text){
     $text = str_replace("y","у",$text);
     $text = str_replace("ў","ӯ",$text);
     $text = str_replace("й","й",$text);
@@ -251,5 +284,90 @@
     
      return $text; 
  }
+ 
+  function remove_erab($text){
+  $text = str_replace("ّ","",$text);
+  $text = str_replace("ِ","",$text);
+  $text = str_replace("ُ","",$text);
+  $text = str_replace("َ","",$text);
+  $text = str_replace("ٍ","",$text);
+  $text = str_replace("ٌ","",$text);
+  $text = str_replace("ً","",$text);
+  
+  return $text;
+  }
+ 
+  function correct_tashdid($text){
+    $text = str_replace("آا","آ",$text);  
+    $text = str_replace("آآ","آ",$text);
+    $text = str_replace("اا","ا",$text);
+    $text = str_replace("بب","ب",$text);
+    $text = str_replace("پپ","پ",$text);
+    $text = str_replace("تت","ت",$text);
+    $text = str_replace("جج","ج",$text);
+    $text = str_replace("چچ","چ",$text);
+    $text = str_replace("هه","ه",$text);
+    $text = str_replace("خخ","خ",$text);
+    $text = str_replace("دد","د",$text);
+    $text = str_replace("زز","ز",$text);
+    $text = str_replace("رر","ر",$text);
+    $text = str_replace("ژژ","ز",$text);
+    $text = str_replace("سس","س",$text);
+    $text = str_replace("شش","ش",$text);
+    $text = str_replace("عع","ع",$text);
+    $text = str_replace("غغ","غ",$text);
+    $text = str_replace("فف","ف",$text);
+    $text = str_replace("قق","ق",$text);
+    $text = str_replace("کک","ک",$text);
+    $text = str_replace("گگ","گ",$text);
+    $text = str_replace("لل","ل",$text);
+    $text = str_replace("مم","م",$text);
+    $text = str_replace("وو","و",$text);
+    $text = str_replace("یی","ی",$text);
+    
+    return $text; 
+ }
+ 
+  function filter_common_words($text){
+    $text = str_replace("مسایل","مسائل",$text);
+    $text = str_replace("داخیل","داخل",$text);
+    $text = str_replace("تاجیکیستان","تاجیکستان",$text);
+    $text = str_replace("بِشتر","بیشتر",$text);
+    $text = str_replace("مُسلله","مسلح",$text);
+    $text = str_replace("ایدامه","ادامه",$text);
+    $text = str_replace("توانیست","توانست",$text);
+    $text = str_replace("بیدیهد","بدهد",$text);
+    $text = str_replace("دیهد","دهد",$text);
+    $text = str_replace("ایرای","ارائه",$text);
+    $text = str_replace("بلکی","بلکه",$text);
+    $text = str_replace("تنووُع","تنوع",$text);
+    $text = str_replace("فرهنگیی"," فرهنگی",$text);
+    $text = str_replace("افزُد","افزود",$text);
+    $text = str_replace("شُنوندگان","شنوندگان",$text);
+    $text = str_replace(",","،",$text);
+    $text = str_replace("بیده","بده",$text);
+    $text = str_replace("رادییو","رادیو",$text);
+    $text = str_replace("دُشنبِ","دوشنبه",$text);
+    $text = str_replace("شیمال","شمال",$text);
+    $text = str_replace("جینوب","جنوب",$text);
+    $text = str_replace("امما","اما",$text);
+    $text = str_replace("مهلل","محل",$text);
+    $text = str_replace("زییاد","زیاد",$text);
+    $text = str_replace("مِش","می ش",$text);
+    $text = str_replace("بُد","بود",$text);
+    $text = str_replace("هُرُف","حروف",$text);
+    $text = str_replace("بعدن","بعدا",$text);
+    $text = str_replace("اوول","اول",$text);
+    $text = preg_replace('/^' . preg_quote('مِ', '/') . '/', 'می', $text);
+    $text = preg_replace('/^' . preg_quote('نِ', '/') . '/', 'نی', $text);
+    $text = str_replace("ختی","خط",$text);
+    $text = str_replace("هُدُد","حدود",$text);
+    $text = str_replace("خاه","خواه",$text);
+    $text = str_replace("مربُت","مربوط",$text);
+    $text = str_replace("کیشور","کشور",$text);
+    $text = str_replace("دیه","ده",$text);
+    
+     return $text; 
+  }
  
 ?>
